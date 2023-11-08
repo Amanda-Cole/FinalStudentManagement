eee22414cf8cd965e37d171c9e5e7f208323a443
Amanda-Cole <col22031@byui.edu>
2023-11-08 15:54:13 -0700

    All queries working

--- main.py
+++ main.py
@@ -1,334 +1,137 @@
-# # import tkinter as tk
-# # import sqlite3
-# # from tkinter import ttk, messagebox
-
-# # root = tk.Tk()
-# # root.title("Management")
-
-# # connection = sqlite3.connect("sprint4_students.db")
-# # cursor = connection.cursor()
-
-# # TABLE_NAME = "students"
-# # STUDENT_ID = "students_id"
-# # FIRST_NAME = "first_name"
-# # LAST_NAME = "last_name"
-# # GRADE = "grade"
-# # GPA = "gpa"
-
-# # class Student:
-# #     first_name = ""
-# #     last_name = ""
-# #     grade = ""
-# #     gpa =""
-
-# #     def __init__(self,first_name,last_name,grade,gpa):
-# #         self.first_name = first_name
-# #         self.last_name = last_name
-# #         self.grade = grade
-# #         self.gpa = gpa
-        
-
-# # #Build form basics
-# # appLabel = tk.Label(root, text="Student Management System", fg = "#06a099", width = 35)
-# # appLabel.config(font = ("Sylfaen",30))
-# # appLabel.grid(row=0, columnspan=2, padx=(10,10), pady=(30,0))
-
-# # #Create Labels for input
-# # fnameLabel = tk.Label(root, text="Student First Name", width=40, anchor = 'w',
-# #                      font = ("Sylfaen",12)).grid(row=1, column=0, padx=(10,0),
-# #                      pady=(30,0))
-# # lnameLabel = tk.Label(root, text="Student Last Name", width=40, anchor = 'w',
-# #                      font = ("Sylfaen",12)).grid(row=2, column=0, padx=(10,0),
-# #                      pady=(30,0))
-# # gradeLabel = tk.Label(root, text="Student Grade", width=40, anchor = 'w',
-# #                      font = ("Sylfaen",12)).grid(row=3, column=0, padx=(10,0),
-# #                      pady=(30,0))
-# # gpaLabel = tk.Label(root, text="Student GPA", width=40, anchor = 'w',
-# #                      font = ("Sylfaen",12)).grid(row=4, column=0, padx=(10,0),
-# #                      pady=(30,0))
-
-# # #Create input boxes.  Grid shows place on form
-# # fnameEntry = tk.Entry(root, width=30)
-# # fnameEntry.grid(row=1, column=1,padx=(0,10),pady=(30,20))
-# # lnameEntry = tk.Entry(root, width=30)
-# # lnameEntry.grid(row=2, column=1,padx=(0,10),pady=(30,20))
-# # gradeEntry = tk.Entry(root, width=30)
-# # gradeEntry.grid(row=3, column=1,padx=(0,10),pady=(30,20))
-# # gpaEntry = tk.Entry(root, width=30)
-# # gpaEntry.grid(row=4, column=1,padx=(0,10),pady=(30,20))
-
-
-# # def addStudentInput():
-# #     # global fnameEntry, lnameEntry, gradeEntry, gpaEntry
-# #     # global list
-# #     # global TABLE_NAME, FIRST_NAME, LAST_NAME, GRADE, GPA
-# #     firstname = fnameEntry.get()
-# #     fnameEntry.delete(0, tk.END)
-# #     lastname = lnameEntry.get()
-# #     lnameEntry.delete(0,tk.END)
-# #     grade = int(gradeEntry.get())
-# #     gradeEntry.delete(0, tk.END)
-# #     gpa = double(gpaEntry.get())
-# #     gpaEntry.delete(0, tk.END)
-    
-    
-# #     #INSERT INTO DATABASE
-# #     connection = sqlite3.connect('sprint4_students.db')
-# #     cursor = connection.cursor()
-    
-# #     cursor.execute(("INSERT INTO students (first_name, last_name, grade, gpa) VALUES (?, ?, ?, ?)",
-# #                    (firstname, lastname, grade, gpa)))
-    
-
-# #     connection.commit()
-# #     messagebox.showinfo("Success, Student Added")
- 
-
-
-
-# # button = tk.Button(root, text="Add Student", command=lambda :addStudentInput())
-# # button.grid(row=5, column=0, pady=20)
-
-# # # displayButton = tk.Button(root, text="Display Students", command=lamda : destroyRootWindow())
-# # # displayButton.grid(row=5, column=1)
-
-
-
-
-
-# # #Starts build
-# # root.mainloop()
-
-# import tkinter as tk
-# from tkinter import messagebox
-# import mysql.connector
-
-# root = tk.Tk()
-# root.title("Management")
-
-
-
-# def create_connection():
-#     return mysql.connector.connect(
-#         host="localhost",
-#         user="root",
-#         password="Austin25$",
-#         database="sprint4_students"
-# )
-
-
-# class Student:
-#     def __init__(self, first_name, last_name, grade, gpa):
-#         self.first_name = first_name
-#         self.last_name = last_name
-#         self.grade = grade
-#         self.gpa = gpa
-
-# connection = create_connection()
-# cursor = connection.cursor()     
-
-# # Your Tkinter GUI code...
-# # #Build form basics
-# appLabel = tk.Label(root, text="Student Management System", fg = "#06a099", width = 35)
-# appLabel.config(font = ("Sylfaen",30))
-# appLabel.grid(row=0, columnspan=2, padx=(10,10), pady=(30,0))
-
-# #Create Labels for input
-# fnameLabel = tk.Label(root, text="Student First Name", width=40, anchor = 'w',
-#                      font = ("Sylfaen",12)).grid(row=1, column=0, padx=(10,0),
-#                      pady=(30,0))
-# lnameLabel = tk.Label(root, text="Student Last Name", width=40, anchor = 'w',
-#                      font = ("Sylfaen",12)).grid(row=2, column=0, padx=(10,0),
-#                      pady=(30,0))
-# gradeLabel = tk.Label(root, text="Student Grade", width=40, anchor = 'w',
-#                      font = ("Sylfaen",12)).grid(row=3, column=0, padx=(10,0),
-#                      pady=(30,0))
-# gpaLabel = tk.Label(root, text="Student GPA", width=40, anchor = 'w',
-#                      font = ("Sylfaen",12)).grid(row=4, column=0, padx=(10,0),
-#                      pady=(30,0))
-
-# #Create input boxes.  Grid shows place on form
-# fnameEntry = tk.Entry(root, width=30)
-# fnameEntry.grid(row=1, column=1,padx=(0,10),pady=(30,20))
-# lnameEntry = tk.Entry(root, width=30)
-# lnameEntry.grid(row=2, column=1,padx=(0,10),pady=(30,20))
-# gradeEntry = tk.Entry(root, width=30)
-# gradeEntry.grid(row=3, column=1,padx=(0,10),pady=(30,20))
-# gpaEntry = tk.Entry(root, width=30)
-# gpaEntry.grid(row=4, column=1,padx=(0,10),pady=(30,20))
-
-
-# def addStudentInput():
-#     try:
-#         firstname = fnameEntry.get()
-#         fnameEntry.delete(0, tk.END)
-#         lastname = lnameEntry.get()
-#         lnameEntry.delete(0, tk.END)
-#         grade = int(gradeEntry.get())
-#         gradeEntry.delete(0, tk.END)
-#         gpa = float(gpaEntry.get())
-#         gpaEntry.delete(0, tk.END)
-        
-#         # INSERT INTO DATABASE
-#         insert_query = "INSERT INTO students (first_name, last_name, grade, gpa) VALUES (%s, %s, %s, %s)"
-#         cursor.execute(insert_query, (firstname, lastname, grade, gpa))
-#         connection.commit()
-#         messagebox.showinfo("Success", "Student Added Successfully")
-#     except ValueError:
-#         messagebox.showerror("Error", "Invalid input. Please enter valid data.")
-        
-# def displayStudents():
-#     try:
-#         global cursor, connection  # Accessing the global cursor and connection
-
-#         # Check if connection is closed, then re-establish the connection
-#         if not connection.is_connected():
-#             connection = create_connection()
-#             cursor = connection.cursor()
-
-#         select_query = "SELECT * FROM students"
-
-#         cursor.execute(select_query)
-#         students = cursor.fetchall()
-
-#         display_window = tk.Toplevel(root)
-#         display_window.title("Student Records")
-
-#         if students:
-#             for i, student in enumerate(students):
-#                 student_info = f"ID: {student[0]}, Name: {student[1]} {student[2]}, Grade: {student[3]}, GPA: {student[4]}"
-#                 label = tk.Label(display_window, text=student_info)
-#                 label.grid(row=i, column=0, padx=10, pady=5)
-#         else:
-#             messagebox.showinfo("No Students", "No student records found.")
-#     except mysql.connector.Error as e:
-#         messagebox.showerror("Error", f"MySQL Error: {e}")
-
-          
-# try:
-#     button = tk.Button(root, text="Add Student", command=addStudentInput)
-#     button.grid(row=5, column=0, pady=20)
-#     connection.commit()
-# except mysql.connector.Error as error:
-#     print("Failed to insert record into MySQL table:", error)
-# finally:
-#     if connection.is_connected():
-#         cursor.close()
-#         connection.close()
-  
-  
-        
-# try:
-#     display_button = tk.Button(root, text="Display Students", command=displayStudents)
-#     display_button.grid(row=6, column=0, pady=20)
-#     connection.commit()
-# except mysql.connector.Error as error:
-#     print("Failed to display from MySQL table:", error)
-# finally:
-#     if connection.is_connected():
-#         cursor.close()
-#         connection.close()
-
-# root.mainloop()
-import tkinter as tk
 import mysql.connector
-from tkinter import messagebox
 
-def create_connection():
-    return mysql.connector.connect(
+#connection to mysql workbench database
+connection = mysql.connector.connect(
         host="localhost",
         user="root",
         password="Austin25$",
         database="sprint4_students"
     )
-
-def addStudentInput():
-    try:
-        firstname = fnameEntry.get()
-        fnameEntry.delete(0, tk.END)
-        lastname = lnameEntry.get()
-        lnameEntry.delete(0, tk.END)
-        grade = int(gradeEntry.get())
-        gradeEntry.delete(0, tk.END)
-        gpa = float(gpaEntry.get())
-        gpaEntry.delete(0, tk.END)
-        
-        # INSERT INTO DATABASE
-        insert_query = "INSERT INTO students (first_name, last_name, grade, gpa) VALUES (%s, %s, %s, %s)"
-        cursor.execute(insert_query, (firstname, lastname, grade, gpa))
-        connection.commit()
-        messagebox.showinfo("Success", "Student Added Successfully")
-    except ValueError as e:
-        messagebox.showerror("Error", f"Invalid input: {e}")
-
-def displayStudents():
-    try:
-        global cursor, connection
-        
-        # Check if the connection is closed, then re-establish the connection
-        if not connection.is_connected():
-            connection = create_connection()
-            cursor = connection.cursor()
-
-        select_query = "SELECT * FROM students"
-        cursor.execute(select_query)
-        students = cursor.fetchall()
-
-        display_window = tk.Toplevel(root)
-        display_window.title("Student Records")
-
-        if students:
-            for i, student in enumerate(students):
-                student_info = f"ID: {student[0]}, Name: {student[1]} {student[2]}, Grade: {student[3]}, GPA: {student[4]}"
-                label = tk.Label(display_window, text=student_info)
-                label.grid(row=i, column=0, padx=10, pady=5)
-        else:
-            messagebox.showinfo("No Students", "No student records found.")
-    except mysql.connector.Error as e:
-        messagebox.showerror("Error", f"MySQL Error: {e}")
-
-# Initialize Tkinter window
-root = tk.Tk()
-root.title("Management")
-
-# Establish initial database connection and cursor
-connection = create_connection()
+# Create a cursor object using the connection
 cursor = connection.cursor()
 
-# Your Tkinter GUI code...
-appLabel = tk.Label(root, text="Student Management System", fg="#06a099", width=35)
-appLabel.config(font=("Sylfaen", 30))
-appLabel.grid(row=0, columnspan=2, padx=(10, 10), pady=(30, 0))
+# Function to display data from a table
+def display_data():
+    print("------------------------------------------------------")
+    query = "SELECT * FROM students"
+    cursor.execute(query)
+    result = cursor.fetchall()
+    for row in result:
+        print(row)
+    print("------------------------------------------------------")
+    
+# Function to add data to a table
+def add_data():
+    print("Enter data to add:")
+    first_name = input("Enter value for First Name: ")
+    last_name = input("Enter value for Last Name: ")
+    grade = input("Enter value for Grade: ")
+    gpa = input("Enter value for GPA: ")
 
-fnameLabel = tk.Label(root, text="Student First Name", width=40, anchor='w', font=("Sylfaen", 12))
-fnameLabel.grid(row=1, column=0, padx=(10, 0), pady=(30, 0))
+    query = "INSERT INTO students (first_name, last_name, grade, gpa) VALUES (%s, %s, %s, %s)"
+    cursor.execute(query, (first_name, last_name, grade, gpa))
+    connection.commit()
+    print()
+    display_data()
+    print()
+    print("Data added successfully")
 
-lnameLabel = tk.Label(root, text="Student Last Name", width=40, anchor='w', font=("Sylfaen", 12))
-lnameLabel.grid(row=2, column=0, padx=(10, 0), pady=(30, 0))
+# Function to edit data in a table
+def edit_data(edit_id):
+    select_query = "SELECT first_name, last_name, grade, gpa FROM students WHERE students_id = %s"
+    cursor.execute(select_query,(edit_id,))
+    result = cursor.fetchall()
+    for row in result:
+        print(row)
+    print()
+    print("Enter your Updates:")
+    new_firstname = input("Enter new value for First Name: ")
+    new_lastname = input("Enter new value for Last Name: ")
+    new_grade = input("Enter new value for Grade: ")
+    new_gpa = input("Enter new value for GPA: ")
 
-gradeLabel = tk.Label(root, text="Student Grade", width=40, anchor='w', font=("Sylfaen", 12))
-gradeLabel.grid(row=3, column=0, padx=(10, 0), pady=(30, 0))
 
-gpaLabel = tk.Label(root, text="Student GPA", width=40, anchor='w', font=("Sylfaen", 12))
-gpaLabel.grid(row=4, column=0, padx=(10, 0), pady=(30, 0))
+    query = "UPDATE students SET first_name = %s, last_name = %s, grade = %s , gpa = %s WHERE students_id = %s"
+    cursor.execute(query, (new_firstname, new_lastname, new_grade, new_gpa, edit_id))
+    connection.commit()
+    print()
+    display_data()
+    print()
+    print("Data updated successfully")
 
-fnameEntry = tk.Entry(root, width=30)
-fnameEntry.grid(row=1, column=1, padx=(0, 10), pady=(30, 20))
+# Function to delete data from a table
+def delete_data(delete_id):
+    query = "DELETE FROM students WHERE students_id = %s"
+    cursor.execute(query, (delete_id,))
+    connection.commit()
+    print()
+    display_data()
+    print("Data deleted successfully")
+    
+#function to return counselor for selected student
+def retrieve_counselor():
+    display_data()
+    choice_id = input("Which student id are you searching for? ")
+    query = """
+        SELECT c.first_name, c.last_name
+        FROM counselor c
+        JOIN students_has_counselor shc ON c.counselor_id = shc.counselor_id
+        JOIN students s ON s.students_id = shc.students_id
+        WHERE s.students_id = %s
+    """
+    cursor.execute(query, (choice_id,))
+    result = cursor.fetchall()
 
-lnameEntry = tk.Entry(root, width=30)
-lnameEntry.grid(row=2, column=1, padx=(0, 10), pady=(30, 20))
+    if not result:
+        print("No counselor found for the given student ID.")
+    else:
+        print("Counselor Information:")
+        for row in result:
+            print(row)
 
-gradeEntry = tk.Entry(root, width=30)
-gradeEntry.grid(row=3, column=1, padx=(0, 10), pady=(30, 20))
+# Function to show menu options
+def menu():
+    while True:
+        print("\nMenu:")
+        print("1. Display data")
+        print("2. Add data")
+        print("3. Edit data")
+        print("4. Delete data")
+        print("5. Find Counselor")
+        print("6. Exit")
+        print()
+        choice = input("Enter your choice (1-5): ")
+        print()
+        if choice == "1":
+            print()
+            print("Displaying data:")
+            display_data()
+        elif choice == "2":
+            add_data()
+        elif choice == "3":
+            display_data()
+            print()
+            edit_id = input ("What student id would you like to edit?")
+            edit_data(edit_id)
+        elif choice == "4":
+            display_data()
+            print()
+            delete_id = input ("What student id would you like to delete?")
+            delete_data(delete_id)
+        elif choice == "5":
+            print()
+            print("Get Counselor")
+            retrieve_counselor()
 
-gpaEntry = tk.Entry(root, width=30)
-gpaEntry.grid(row=4, column=1, padx=(0, 10), pady=(30, 20))
+        elif choice == "6":
+            break
+        else:
+            print("Invalid choice. Please enter a valid option.")
 
-# Buttons to Add and Display students
-button = tk.Button(root, text="Add Student", command=addStudentInput)
-button.grid(row=5, column=0, pady=20)
+# Run the menu
+print("\033[1;31;40m Student Management System \033[0m")
+menu()
+# Close the cursor and the connection
+cursor.close()
+connection.close()    
 
-display_button = tk.Button(root, text="Display Students", command=displayStudents)
-display_button.grid(row=6, column=0, pady=20)
-
-root.mainloop()
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,100 +1,18 @@
-import tkinter as tk
-# import sqlite3
-# from tkinter import ttk, messagebox
+def addStudentInput():
+#     try:
+#         firstname = fnameEntry.get()
+#         fnameEntry.delete(0, tk.END)
+#         lastname = lnameEntry.get()
+#         lnameEntry.delete(0, tk.END)
+#         grade = int(gradeEntry.get())
+#         gradeEntry.delete(0, tk.END)
+#         gpa = float(gpaEntry.get())
+#         gpaEntry.delete(0, tk.END)
 
-# root = tk.Tk()
-# root.title("Management")
-
-# connection = sqlite3.connect("sprint4_students.db")
-# cursor = connection.cursor()
-
-# TABLE_NAME = "students"
-# STUDENT_ID = "students_id"
-# FIRST_NAME = "first_name"
-# LAST_NAME = "last_name"
-# GRADE = "grade"
-# GPA = "gpa"
-
-# class Student:
-#     first_name = ""
-#     last_name = ""
-#     grade = ""
-#     gpa =""
-
-#     def __init__(self,first_name,last_name,grade,gpa):
-#         self.first_name = first_name
-#         self.last_name = last_name
-#         self.grade = grade
-#         self.gpa = gpa
-        
-
-# #Build form basics
-# appLabel = tk.Label(root, text="Student Management System", fg = "#06a099", width = 35)
-# appLabel.config(font = ("Sylfaen",30))
-# appLabel.grid(row=0, columnspan=2, padx=(10,10), pady=(30,0))
-
-# #Create Labels for input
-# fnameLabel = tk.Label(root, text="Student First Name", width=40, anchor = 'w',
-#                      font = ("Sylfaen",12)).grid(row=1, column=0, padx=(10,0),
-#                      pady=(30,0))
-# lnameLabel = tk.Label(root, text="Student Last Name", width=40, anchor = 'w',
-#                      font = ("Sylfaen",12)).grid(row=2, column=0, padx=(10,0),
-#                      pady=(30,0))
-# gradeLabel = tk.Label(root, text="Student Grade", width=40, anchor = 'w',
-#                      font = ("Sylfaen",12)).grid(row=3, column=0, padx=(10,0),
-#                      pady=(30,0))
-# gpaLabel = tk.Label(root, text="Student GPA", width=40, anchor = 'w',
-#                      font = ("Sylfaen",12)).grid(row=4, column=0, padx=(10,0),
-#                      pady=(30,0))
-
-# #Create input boxes.  Grid shows place on form
-# fnameEntry = tk.Entry(root, width=30)
-# fnameEntry.grid(row=1, column=1,padx=(0,10),pady=(30,20))
-# lnameEntry = tk.Entry(root, width=30)
-# lnameEntry.grid(row=2, column=1,padx=(0,10),pady=(30,20))
-# gradeEntry = tk.Entry(root, width=30)
-# gradeEntry.grid(row=3, column=1,padx=(0,10),pady=(30,20))
-# gpaEntry = tk.Entry(root, width=30)
-# gpaEntry.grid(row=4, column=1,padx=(0,10),pady=(30,20))
-
-
-# def addStudentInput():
-#     # global fnameEntry, lnameEntry, gradeEntry, gpaEntry
-#     # global list
-#     # global TABLE_NAME, FIRST_NAME, LAST_NAME, GRADE, GPA
-#     firstname = fnameEntry.get()
-#     fnameEntry.delete(0, tk.END)
-#     lastname = lnameEntry.get()
-#     lnameEntry.delete(0,tk.END)
-#     grade = int(gradeEntry.get())
-#     gradeEntry.delete(0, tk.END)
-#     gpa = double(gpaEntry.get())
-#     gpaEntry.delete(0, tk.END)
-    
-    
-#     #INSERT INTO DATABASE
-#     connection = sqlite3.connect('sprint4_students.db')
-#     cursor = connection.cursor()
-    
-#     cursor.execute(("INSERT INTO students (first_name, last_name, grade, gpa) VALUES (?, ?, ?, ?)",
-#                    (firstname, lastname, grade, gpa)))
-    
-
-#     connection.commit()
-#     messagebox.showinfo("Success, Student Added")
- 
-
-
-
-# button = tk.Button(root, text="Add Student", command=lambda :addStudentInput())
-# button.grid(row=5, column=0, pady=20)
-
-# # displayButton = tk.Button(root, text="Display Students", command=lamda : destroyRootWindow())
-# # displayButton.grid(row=5, column=1)
-
-
-
-
-
-# #Starts build
-# root.mainloop()
+#         # INSERT INTO DATABASE
+#         insert_query = "INSERT INTO students (first_name, last_name, grade, gpa) VALUES (%s, %s, %s, %s)"
+#         cursor.execute(insert_query, (firstname, lastname, grade, gpa))
+#         connection.commit()
+#         messagebox.showinfo("Success", "Student Added Successfully")
+#     except ValueError as e:
+#         messagebox.showerror("Error", f"Invalid input: {e}")
